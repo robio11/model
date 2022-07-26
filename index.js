a4adfb81bfd952ac90c19795b3557f5f2bc72ec5
Rachana Meena <100551236+robio11@users.noreply.github.com>
2022-07-26 13:44:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("model");
+
+let model = document.querySelector(".btn");
+let Close = document.querySelector(".close-btn");
+
+model.addEventListener('click',function()  {
+model.classList.style = '.btn';
+
+})
